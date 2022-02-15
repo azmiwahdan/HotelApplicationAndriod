f0f7d809e06b39fa584332245ef2bebe49837787
Mohammad Anan <smoker_shuger@hotmail.com>
2022-02-15 21:46:11 +0200

    pull

---
+++ app/src/main/java/edu/birzeit/hotelproject/controller/SearchRoomActivity.java
@@ -0,0 +1,16 @@
+package edu.birzeit.hotelproject.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.birzeit.hotelproject.R;
+
+public class SearchRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_room);
+    }
+}
